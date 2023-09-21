ad94df0ff4d794411be0d08f0ceba87bf5de4cdd
Bernardo Cardoso <bernardo.cardoso@outsystems.com>
2023-09-21 11:11:57 +0100

    Updated code

--- dist/prod.OutSystemsUI.js
+++ dist/prod.OutSystemsUI.js
@@ -5,23 +5,25 @@ var MyJSFramework;
     (function (BottomSheet) {
         OutSystems.OSUI.Patterns.BottomSheetAPI.Initialize = CustomInitialize;
         function CustomInitialize(bottomSheetId) {
-            const bottomSheet = OutSystems.OSUI.Patterns.BottomSheetAPI.GetBottomSheetItemById(bottomSheetId);
-            const myBottomSheet = new MyBottomSheet(bottomSheet);
-            myBottomSheet.build();
-            return myBottomSheet;
+            const _bottomSheet = OutSystems.OSUI.Patterns.BottomSheetAPI.GetBottomSheetItemById(bottomSheetId);
+            const _myBottomSheet = new MyBottomSheet(_bottomSheet);
+            _myBottomSheet.build();
+            return _myBottomSheet;
         }
         BottomSheet.CustomInitialize = CustomInitialize;
         class MyBottomSheet {
             constructor(bottomSheet) {
                 this._osuiBottomSheet = bottomSheet;
             }
+            _onOverlayClickHandler() {
+                this._osuiBottomSheet.close();
+            }
             build() {
                 this._osuiBottomSheet.build();
+                this._onOverlayClickCallback = this._onOverlayClickHandler.bind(this);
                 this._bottomSheetBlockElement = OSFramework.OSUI.Helper.Dom.GetElementById(this._osuiBottomSheet.widgetId);
-                this._bottomSheetOverlayElem = this._bottomSheetBlockElement.querySelector('.osui-bottom-sheet-overlay');
-                this._bottomSheetOverlayElem.addEventListener('click', () => {
-                    this._osuiBottomSheet.close();
-                });
+                this._bottomSheetOverlayElem = OSFramework.OSUI.Helper.Dom.ClassSelector(this._bottomSheetBlockElement, OSFramework.OSUI.Patterns.BottomSheet.Enum.CssClass.PatternOverlay);
+                this._bottomSheetOverlayElem.addEventListener(OSFramework.OSUI.GlobalEnum.HTMLEvent.Click, this._onOverlayClickCallback);
             }
         }
         BottomSheet.MyBottomSheet = MyBottomSheet;
--- src/scripts/demo.ts
+++ src/scripts/demo.ts
@@ -6,13 +6,13 @@ namespace MyJSFramework.BottomSheet {
 
     // My custom Initialize BottomSheet method
     export function CustomInitialize(bottomSheetId: string): any {
-        const bottomSheet = OutSystems.OSUI.Patterns.BottomSheetAPI.GetBottomSheetItemById(bottomSheetId);
+        const _bottomSheet = OutSystems.OSUI.Patterns.BottomSheetAPI.GetBottomSheetItemById(bottomSheetId);
 
-        const myBottomSheet = new MyBottomSheet(bottomSheet);
+        const _myBottomSheet = new MyBottomSheet(_bottomSheet);
 
-        myBottomSheet.build();
+        _myBottomSheet.build();
 
-        return myBottomSheet;
+        return _myBottomSheet;
     }
 
     // My custom BottomSheet Class 
@@ -23,26 +23,38 @@ namespace MyJSFramework.BottomSheet {
         private _bottomSheetBlockElement: HTMLElement;
         // BottomSheet Overlay Element
         private _bottomSheetOverlayElem: HTMLElement;
+        // Store the click callback
+        private _onOverlayClickCallback: OSFramework.OSUI.GlobalCallbacks.Generic;
 
         constructor(bottomSheet: OSFramework.OSUI.Patterns.BottomSheet.IBottomSheet) {
             this._osuiBottomSheet = bottomSheet;
         }
 
+        // Method to handle the Click overlay
+        private _onOverlayClickHandler(): void {
+            this._osuiBottomSheet.close();
+        }
+
+        /**
+         * Method to build the MyBottomSheet
+         *
+         * @memberof MyBottomSheet
+         */
         public build() {
             // Build the OutSystems UI BottomSheet
             this._osuiBottomSheet.build();
+
+            // Set the overlay callback
+            this._onOverlayClickCallback = this._onOverlayClickHandler.bind(this);
 
             // Get the BottomSheet block element
             this._bottomSheetBlockElement = OSFramework.OSUI.Helper.Dom.GetElementById(this._osuiBottomSheet.widgetId);
 
             // Get the BottomSheet Overlay element
-            // this._bottomSheetOverlayElem = OSFramework.OSUI.Helper.Dom.ClassSelector(this._bottomSheetBlockElement, OSFramework.OSUI.Patterns.BottomSheet.Enum.CssClass.PatternOverlay);
-            this._bottomSheetOverlayElem = this._bottomSheetBlockElement.querySelector('.osui-bottom-sheet-overlay');
+            this._bottomSheetOverlayElem = OSFramework.OSUI.Helper.Dom.ClassSelector(this._bottomSheetBlockElement, OSFramework.OSUI.Patterns.BottomSheet.Enum.CssClass.PatternOverlay);
 
             // Add the click event on the Overlay element
-            this._bottomSheetOverlayElem.addEventListener('click', () => {
-                // Close the BottomSheet
-            })
+            this._bottomSheetOverlayElem.addEventListener(OSFramework.OSUI.GlobalEnum.HTMLEvent.Click, this._onOverlayClickCallback);
         }
     }
 }
